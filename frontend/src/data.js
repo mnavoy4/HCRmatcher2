93cb1d3dd582bf07fd9b3bc72e731ccd4440a9d5
mnavoy4 <mnavoy4@gmail.com>
2020-11-15 18:22:26 -0700

    dummy candidate data

---
+++ frontend/src/data.js
@@ -0,0 +1,134 @@
+export default {
+  candidates: [
+    {
+      name: "Michael Navoy",
+      location: 'Denver, CO',
+      wantsRemote: false,
+      willingToRelocate: false,
+      skills: [
+        {
+          name: 'Python',
+          experienceInYears: 3
+        },
+        {
+          name: 'Java',
+          experienceInYears: 6
+        },
+        {
+          name: 'Ruby',
+          experienceInYears: 2
+        },
+      ],
+      industriesWorkedIn: ["finance", "healthcare", "cyber security"],
+      usCitizen: true,
+      clearance: {
+        haveOrNot: false,
+        type: 'none'
+      }
+    },
+    {
+      name: "Shaun Coe",
+      location: 'RINO, CO',
+      wantsRemote: true,
+      willingToRelocate: false,
+      skills: [
+        {
+          name: 'Python',
+          experienceInYears: 10
+        },
+        {
+          name: 'SQL',
+          experienceInYears: 6
+        },
+        {
+          name: 'Ruby on Rails',
+          experienceInYears: 2
+        },
+      ],
+      industriesWorkedIn: ["finance", "healthcare", "cyber security"],
+      usCitizen: true,
+      clearance: {
+        haveOrNot: false,
+        type: 'none'
+      }
+    },
+    {
+      name: "Michael Navoy",
+      location: 'Denver, CO',
+      wantsRemote: false,
+      willingToRelocate: false,
+      skills: [
+        {
+          name: 'C+',
+          experienceInYears: 3
+        },
+        {
+          name: 'Java',
+          experienceInYears: 6
+        },
+        {
+          name: 'NodeJS',
+          experienceInYears: 19
+        },
+      ],
+      industriesWorkedIn: ["finance", "healthcare", "cyber security"],
+      usCitizen: true,
+      clearance: {
+        haveOrNot: false,
+        type: 'none'
+      }
+    },
+    {
+      name: "John Basar",
+      location: 'Denver, CO',
+      wantsRemote: false,
+      willingToRelocate: false,
+      languages: [
+        {
+          name: 'Pandas',
+          experienceInYears: 3
+        },
+        {
+          name: 'C/C++',
+          experienceInYears: 6
+        },
+        {
+          name: 'React',
+          experienceInYears: 2
+        },
+      ],
+      industriesWorkedIn: ["finance", "healthcare", "cyber security"],
+      usCitizen: true,
+      clearance: {
+        haveOrNot: false,
+        type: 'none'
+      }
+    },
+    {
+      name: "Perry Martin",
+      location: 'Boulder, CO',
+      wantsRemote: true,
+      willingToRelocate: false,
+      skills: [
+        {
+          name: 'PHP',
+          experienceInYears: 2
+        },
+        {
+          name: 'JavaScript',
+          experienceInYears: 10
+        },
+        {
+          name: 'Ruby',
+          experienceInYears: 2
+        },
+      ],
+      industriesWorkedIn: ["finance", "startup", "cyber security"],
+      usCitizen: true,
+      clearance: {
+        haveOrNot: false,
+        type: 'none'
+      }
+    },
+  ]
+}
