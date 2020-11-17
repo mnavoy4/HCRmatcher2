b44ebcd978bb528c38d261cb90d01625639e9746
mnavoy4 <mnavoy4@gmail.com>
2020-11-16 20:26:02 -0700

    routed to all jobs page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import AllCadidatesContainer from './Containers/AllCandidatesContainer';
+import AllJobsContainer from './Containers/AllJobsContainer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <div className='main'>
           <div className='content'>
             <Route exact={true} path='/' component={AllCadidatesContainer}/>
+            <Route path='/jobs' component={AllJobsContainer} />
           </div>
         </div>
       </div>
---
+++ frontend/src/Containers/AllJobsContainer.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+export default function AllJobsContainer(){
+  return (
+    <div>
+      <h1>All Jobs</h1>
+    </div>
+  )
+}
