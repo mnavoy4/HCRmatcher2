befc5f52cf89f6eb544a1deb0c1972516033fabe
mnavoy4 <mnavoy4@gmail.com>
2020-11-20 10:12:58 -0700

    server connection made and connected to mongo with npm start command

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+// const usersRouter = require('./routes/userRoutes');
+// const jobsRouter = require('./routes/jobsRoutes');
+// const candidatesRouter = require('./routes/candidateRoutes');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const mongoDbUrl = process.env.MONGODB_URL;
+mongoose.connect(mongoDbUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB connection made")
+})
+// app.use('/jobs', jobsRouter);
+// app.use('./candidates', candidatesRouter)
+// app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log('server is listening on port', port)
+})
