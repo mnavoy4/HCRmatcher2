f3a01e2a17b23921f8b542c7f69ef471584006df
mnavoy4 <mnavoy4@gmail.com>
2020-11-21 10:41:29 -0700

    built out candidate and job models

--- backend/models/candidateModel.js
+++ backend/models/candidateModel.js
@@ -4,10 +4,49 @@ const candidateSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   phoneNumber: { type: String, required: false, unique: true },
   city: { type: String },
-  state: { type: String },
+  state: { type: String, required: true },
   wantsRemote: { type: Boolean },
-  openToRelocation: { type: Boolean },
-  usCitizen: { type: Boolean },
-  clearance: { type: String },
-  industriesWorkedIn: { type: Array }
+  openToRelocation: { type: Boolean, required: true },
+  willingToGo: { type: Array },
+  usCitizen: { type: Boolean, required: true },
+  clearance: { type: String, enum: ["None", "Secret", "Public Trust", "Top Secret", "TS/SCI", "Confidential"] },
+  industriesWorkedIn: { type: Array, required: true },
+  companiesWorkedFor: { type: Array },
+  startupExperience: { type: Boolean },
+  skills: { type: Array, required: true },
+  tags: { type: Array },
+  currentSalary: { type: Number },
+  degrees: { type: Array, required: true },
+  certifications: { type: Array },
+  jobTitles: { type: Array, required: true },
+  stageInProcess: { type: String, enum: ["Submitted", "Client Interview", "Zoom Screen", "Hired", "Not Hired", "Offer Made"], required: true },
+  jobsAppliedFor: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Job"
+  }],
+  jobsMatchedFor: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Job"
+  }]
+});
+
+function capitalize(string){
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function capitalizeEachWord(string){
+  return string.split(' ').map(capitalize).join(' ');
+}
+
+candidateSchema.pre('save', function(next){
+  capitalizeEachWord(this.name);
+  capitalizeEachWord(this.city);
+  capitalizeEachWord(this.state);
+  capitalizeEachWord(this.clearance);
+  capitalizeEachWord(this.stageInProcess);
+  next();
 })
+
+const candidateModel = mongoose.model("Candidate", candidateSchema);
+
+module.exports = candidateModel;
---
+++ backend/models/jobModel.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const jobSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  industries: { type: Array },
+  remote: { type: Boolean },
+  jobDescription: { type: String, required: true },
+  candidates: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Candidate"
+  }],
+  willingToSponsor: { type: Boolean, required: true },
+  relocationAssistance: { type: Boolean, required: true},
+  client: { type: String },
+  salaryRange: { type: String, required: true },
+  skillsRequired: { type: Array, required: true },
+  degreesRequired: { type: Array, required: true},
+  tags: { type: Array },
+  recruiter: { type: String },
+  status: { type: String, enum: ['Open', 'Closed', 'Paused'] },
+  employementType: { type: String, enum: ['Full Time', 'Contract', 'Part Time']},
+  clientFee: { type: Number, required: true }
+});
+
+function capitalize(string){
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function capitalizeEachWord(string){
+  return string.split(' ').map(capitalize).join(' ');
+}
+
+jobSchema.pre('save', function(next){
+  capitalizeEachWord(this.title);
+  capitalizeEachWord(this.city);
+  capitalizeEachWord(this.state);
+  capitalizeEachWord(this.client);
+  capitalizeEachWord(this.recruiter);
+  capitalizeEachWord(this.employementType);
+  capitalizeEachWord(this.status);
+  next();
+})
+
+const jobModel = mongoose.model('Job', jobSchema);
+
+module.exports = jobModel;
