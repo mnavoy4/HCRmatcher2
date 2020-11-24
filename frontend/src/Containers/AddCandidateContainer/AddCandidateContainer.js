7fb0728ee5a62b4dfbf0096c48a9eaaadec08175
mnavoy4 <mnavoy4@gmail.com>
2020-11-24 11:16:11 -0700

    building out new candidate form

--- frontend/src/Containers/AddCandidateContainer/AddCandidateContainer.js
+++ frontend/src/Containers/AddCandidateContainer/AddCandidateContainer.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Input, Grid, Button, Container, Checkbox, TextField } from '@material-ui/core';
+
 
 export default function AddCandidateContainer(){
+
+  const { register, handleSubmit } = useForm()
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
       <header className='add-candidates-header'>
         <h1>Add Candidate</h1>
       </header>
       <main>
-        <form>
-          
-        </form>
+        <Grid container spacing={12}>
+          <form>
+            <TextField
+              label='Name'
+            />
+          </form>
+        </Grid>
+ 
       </main>
     </div>
   )
