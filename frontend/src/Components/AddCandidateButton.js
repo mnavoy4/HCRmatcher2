a65ab50ee1500102478218522889d4c5fd0d74fe
mnavoy4 <mnavoy4@gmail.com>
2020-12-02 08:59:41 -0700

    name on candidate table is a link to candidate page

--- frontend/src/Components/AddCandidateButton.js
+++ frontend/src/Components/AddCandidateButton.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -19,7 +20,9 @@ export default function AddCandidateButton(){
         className={classes.button}
         startIcon={<CloudUploadIcon />}
       >
+        <Link to='/add-candidate' style={{ color: 'whitesmoke' }}>
         Add Candidate
+        </Link>
       </Button>
     </div>
   )
--- frontend/src/Containers/AllCandidatesContainer.js
+++ frontend/src/Containers/AllCandidatesContainer.js
@@ -5,6 +5,9 @@ import NavBar from '../Components/NavBar';
 import axios from 'axios';
 import AddCandidateButton from '../Components/AddCandidateButton';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const baseURL = 'http://localhost:5000/candidates'
 
@@ -24,23 +27,16 @@ export default function AllCadidatesContainer() {
       <NavBar/>
       <h1 className='table-h1'>All Candidates</h1>
       <div className='table-container'>
-        <Link to='/add-candidate'>
-          <AddCandidateButton />
-        </Link>
+        <AddCandidateButton />
         <MaterialTable
           title = "All Candidates"
           icons={tableIcons}
           columns={[
-            { title: "Name", field: 'name' , type: 'string' },
+            { title: "Name", field: 'name' , type: 'string', render: rowData => <Link to={`/candidate/${rowData._id}`}>{rowData.name}</Link> },
             { title: "Location", field: 'state' , type: 'string' },
             { title: "Wants Remote?", field: 'wantsRemote', type: 'boolean' },
             { title: "Willing to relocate?", field: 'openToRelocation', type: 'boolean' },
-            // { title: "Skills", field: 'skills' },
-            // { title: "Industries with Experience", field: 'industriesWorkedIn', render: (rowData) => {
-            //   rowData.industriesWorkedIn.forEach((entry) => {
-            //     return entry.industry
-            //   })
-            // } },
+            // { title: 'Job Applied For', field: 'jobsAppliedFor' },
             { title: "US Citizen", field: "usCitizen", type: 'boolean'},
             { title: "Security Clearance", field: 'clearance' }
           ]}
@@ -52,7 +48,7 @@ export default function AllCadidatesContainer() {
           data={candidateData}
           actions={[
             {
-              icon: 'Delete',
+              icon: () => <FontAwesomeIcon icon={faTrash} />,
               tooltip: 'Delete Candidate',
               onClick: (event, rowData) => alert("You want to delete this candidate " + rowData.name)
             }
