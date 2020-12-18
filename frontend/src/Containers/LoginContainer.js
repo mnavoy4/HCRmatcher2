725c40587c8999dc5c0f1ebae90ce781cdae3ea0
mnavoy4 <mnavoy4@gmail.com>
2020-12-18 12:09:46 -0700

    login page fUI ddone, need to work on login redirect and routes to get token in cookie

--- frontend/src/Containers/LoginContainer.js
+++ frontend/src/Containers/LoginContainer.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useForm, Controller, useFieldArray } from 'react-hook-form';
-import { Grid, TextField, Paper, RadioGroup, FormControlLabel, Checkbox, FormGroup, Radio, FormLabel, FormControl, Button } from '@material-ui/core';
+import { Grid, TextField, Paper, RadioGroup, FormControlLabel, Checkbox, FormGroup, Radio, FormLabel, FormControl, Button, InputAdornment } from '@material-ui/core';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
+import { AccountCircle, LockRounded } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -34,6 +35,9 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     height: '100%',
     objectFit: 'cover'
+  },
+  firstNestedGrid: {
+    padding: 10
   }
 }));
 
@@ -48,10 +52,62 @@ export default function LoginContainer(props){
     <div>
       <Grid container className={classes.mainGridContainer}>
         <Grid item xs={12} sm={6}>
-          <img src='https://images.fineartamerica.com/images-medium-large-5/portrait-of-a-lion-lucie-bilodeau.jpg' className={classes.bigSideImage}/>
+          <img
+            src='https://images.fineartamerica.com/images-medium-large-5/portrait-of-a-lion-lucie-bilodeau.jpg'
+            className={classes.bigSideImage}
+            alt='Brand'
+          />
         </Grid>
-        <Grid item xs={12} sm={6}>
-
+        <Grid
+          container
+          item
+          xs={12}
+          sm={6}
+          className={classes.firstNestedGrid}
+          alignItems='center'
+          direction='column'
+          justify='space-between'
+        >
+          <div></div>
+          <form style={{ display: 'flex', flexDirection: 'column', maxWidth: 400, minWidth: 300 }}>
+            <Grid container justify='center'>
+              <img
+                src='https://static.wixstatic.com/media/206294_1e4e4cb5cc9a47a68248897bcb86c029~mv2.png/v1/fill/w_302,h_65,al_c,q_85,usm_0.66_1.00_0.01/Logo.webp'
+                width={300}
+                alt='Logo'
+              />
+            </Grid>
+            <TextField
+              label='Email'
+              margin='normal'
+              inputRef={register({ required: true })}
+              name='email'
+              InputProps={{ startAdornment: <InputAdornment position='start'><AccountCircle/></InputAdornment>}}
+            />
+            <TextField
+              label='Password'
+              margin='normal'
+              inputRef={register({ required: true })}
+              name='password'
+              InputProps={{ startAdornment: <InputAdornment position='start'><LockRounded/></InputAdornment>}}
+            />
+            <Button
+              type='submit'
+              color='primary'
+              variant='contained'
+            >
+              Login
+            </Button>
+            <Button style={{ marginTop: 10 }}>New User?</Button>
+          </form>
+          <Grid container justify='center' spacing={2}>
+            <Grid item>
+              <Button color='primary'>Go to about page</Button>
+            </Grid>
+            <Grid item>
+              <Button variant='outlined'>Forgot Password?</Button>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </div>
